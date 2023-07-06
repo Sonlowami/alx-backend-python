27fd32444cfcf053691c6957207fafaa7a6ebecb
Sonlowami <uwilowami64@outlook.com>
2023-07-06 23:34:46 +0200

    Use TypeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """This module contain a function that returns a key from a dictionary"""
-from typing import Union, Mapping, Any
+from typing import Union, Mapping, TypeVar
+
+T = TypeVar['T']
 
 
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[Any, None] = None) -> Union[Any, None]:
+def safely_get_value(dct: Mapping[T, T], key: T,
+                     default: Union[Any, None] = None) -> T:
     """Return a key from a dictionary if it is there. Return the default
     otherwise"""
     if key in dct:
