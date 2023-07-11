1efe1adfea9d51b9d88ad34e39708b31586c9f2e
Sonlowami <uwilowami64@outlook.com>
2023-07-11 23:17:51 +0200

    Fix annotation errors

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """wait for a random number of seconds between 0 and max_delay"""
     delay: float = random.uniform(0, max_delay)
     return await asyncio.sleep(delay, result=delay)
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,10 +7,10 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, delay_time: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Count how much on average is an async function taking"""
 
     initial_time: float = time.perf_counter()
-    asyncio.run(wait_n(n, delay_time))
+    asyncio.run(wait_n(n, max_delay))
     final_time: float = time.perf_counter() - initial_time
     return final_time / n
